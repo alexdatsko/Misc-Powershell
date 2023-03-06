0909f792ca97a9694d9fd0b1659117c8d1c23d52
Alex Datsko <alexdatsko@gmail.com>
2023-03-06 10:37:23 -0800

    Check-IPDNSReverseLookup.ps1 added ability to paste IPs in

--- Check-IPDNSReverseLookup.ps1
+++ Check-IPDNSReverseLookup.ps1
@@ -12,11 +12,31 @@ $ips = @()
 #$ips = @("127.0.0.1","8.8.8.8","4.2.2.2")
 
 if ($ips.Length -lt 1) {
+  if (Test-Path $IPFilename) {
     $ips = Get-Content $IPFileName
     if ($verbose) { 
         Write-Host "IPs loaded from file $($IPFilename): " 
         $ips 
     }
+  } else {
+    $input = " "
+    Write-Host "IP addresses could not be read from $($IPFilename).  Entering manually:"
+    while ($input -ne "") {
+      $input = read-host "Enter IP address (or enter if you are done) "
+      if (([int]($input.split('.')[3]) -gt 0) -and ([int]($input.split('.')[3]) -lt 256)) {
+        if ($ips -notcontains $input) {
+          $ips += $input
+          Write-Host "[+] Added $($input)" -ForegroundColor Green
+        } else {
+          Write-Host "[!] $($input) already exists!" -ForegroundColor Red
+        }
+      } else {
+        if ($input -ne "") {
+          Write-Host "[!] Invalid IP address! Please try again.." -ForegroundColor Red
+        }
+      }
+    }
+  }
 }
 
 foreach ($ip in $ips) {
@@ -28,7 +48,7 @@ foreach ($ip in $ips) {
     }
     $response = Invoke-RestMethod "http://ipwho.is/$ip"
     Write-Host "-- [ipwho.is result]: $(($response).continent) $(($response).region) $(($response).city) $(($response).postal) $(($response).connection)"
-    
+
     Write-Verbose "Extended results:"
     Write-Verbose "$response"
 }
