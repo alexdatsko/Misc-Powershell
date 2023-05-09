f9615ce9a2b79da36050eb8e474a6f93317c5ad5
Alex Datsko <alexdatsko@gmail.com>
2023-05-09 08:24:28 -0700

    gp.ps1 commit msg fix

--- gp.ps1
+++ gp.ps1
@@ -1,19 +1,20 @@
 
 param (
-    [string]$msg = "default"
- )
- Write-Host "`n[[ Git Push script - gp.ps1 ]] Alex Datsko" 
-if ("default" -eq $msg) {
+  [string]$msg = "default"
+)
+Write-Host "`n[[ Git Push script - gp.ps1 ]] v0.2 Alex Datsko" 
+if ($msg -eq "default") {
   write-host "`n Error: please give commit message, i.e :`n  gp.ps1 'added kittens'`n"
   exit
 } else {
+  write-host "[.] Commit msg: '$msg'"
   sl "\\synologycms\dropbox\scripts\AlexD Powershell\Misc-Powershell"
   write-host "[.] Pulling"
   git pull
   write-host "[.] Adding *"
   git add *
-  write-host "[.] Committing with msg: '$commitmsg'"
-  git commit -m "$commitmsg"
+  write-host "[.] Committing with msg: '$msg'"
+  git commit -m "$msg"
   write-host "[.] Pushing.."
   git push
   Write-Host "[!] Done! Exiting.`n"
