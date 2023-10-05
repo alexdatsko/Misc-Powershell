fee6460f7f2e99603e2539c2fe49fe4d6a392daa
Alex Datsko <alexdatsko@gmail.com>
2023-10-05 15:24:01 -0700

    updated delete-chromeprofiles.ps1, fix for recursive input required

--- Delete-ChromeProfiles.ps1
+++ Delete-ChromeProfiles.ps1
@@ -12,29 +12,31 @@ $DateTime = Get-Date -Format "yyyy-MM-dd"
 "`n$DateTime ------------------------------" | tee -append $LogFile
 $Users = (GCI C:\Users -Directory).BaseName
 foreach ($User in $Users) {
-  Write-Output "[.] Processing user $User .." | tee -append $LogFile
+  "[.] Processing user $User .." | tee -append $LogFile
   if (Test-Path "C:\Users\$($User)\AppData\Local\Google" -ErrorAction Continue) {
     $ChromeUserData = "C:\Users\$($User)\AppData\Local\Google\Chrome\User Data\Profile*"
     $Folders = (gci $ChromeUserData -ErrorAction SilentlyContinue).FullName
     if ($Folders) {
       Foreach ($Folder in $Folders) {
-        "Checking $($Folder)\Service Worker\CacheStorage\ .." | tee -append $LogFile
+        "[+] Checking $($Folder)\Service Worker\CacheStorage\ .." | tee -append $LogFile
         if (Test-Path "$($Folder)\Service Worker\CacheStorage") {
-          "Listing contents of $($Folder)\Service Worker\CacheStorage\*.*" | tee -append $LogFile
-          $FilesToDelete = (GCI "$($Folder)\Service Worker\CacheStorage\" -Recurse -ErrorAction SilentlyContinue).FullName 
+          "[.] Listing contents of $($Folder)\Service Worker\CacheStorage\*.*" | tee -append $LogFile
+          $FilesToDelete = (GCI "$($Folder)\Service Worker\CacheStorage\" -File -Recurse -ErrorAction SilentlyContinue).FullName 
           #$FilesToDelete | tee -append $LogFile
-          "Removing $($Folder)\Service Worker\CacheStorage\*.*" | tee -append $LogFile
+          "[-] Removing files $($Folder)\Service Worker\CacheStorage\*.*" | tee -append $LogFile
           foreach ($FileToDelete in $FilesToDelete) {
             Remove-Item $FileToDelete -Force -ErrorAction Continue | tee -append $LogFile
           }
+          Remove-Item "$($Folder)\Service Worker\CacheStorage\*" -Recurse -Force -ErrorAction SilentlyContinue | tee -append $LogFile  # Remove folders also
         } else {
-          "No profile data found for $User in $Folder" | tee -append $LogFile
+          "[ ] No profile data found for $User in $Folder" | tee -append $LogFile
         }
       }
     } else {
-      "No chrome user data folder found for $User" | tee -append $LogFile
+      "[ ] No chrome user data folder found for $User" | tee -append $LogFile
     }
   } else {
-    "No chrome folder found for $User or Access Denied.." | tee -append $LogFile
+    "[ ] No chrome folder found for $User or Access Denied.." | tee -append $LogFile
   }
 }
+"[!] Done!"
