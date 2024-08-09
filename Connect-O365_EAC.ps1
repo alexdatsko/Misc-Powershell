cef456f93412b75b9cb7049d851c0f76ae3ce549
Alex Datsko <alexdatsko@gmail.com>
2024-08-09 12:57:51 -0700

    92063	Microsoft 3D Builder Remote Code Execution (RCE) Vulnerability - September 2023 fix3

--- Connect-O365_EAC.ps1
+++ Connect-O365_EAC.ps1
@@ -1,14 +1,24 @@
+$loginname = Read-Host "What is the admin email to login to?"
+
 Set-ExecutionPolicy RemoteSigned
+
+Write-Host "[.] Installing necessary modules..." 
 Install-module AIPService
-Import-module AIPService
 Install-Module -Name ExchangeOnlineManagement
+
+Write-Host "[.] Importing necessary modules..." 
+Import-module AIPService
 Import-Module ExchangeOnlineManagement
 
-Connect-ExchangeOnline -UserPrincipalName admin@domain.com
+Write-Host "[.] Connecting to EAC with $loginname ..." 
+Connect-ExchangeOnline -UserPrincipalName $loginname
 
+Write-Host "[.] Getting credential to connect to AIP Service..." 
 $cred = Get-Credential
+
+Write-Host "[.] Connecting to AIP Service..." 
 Connect-AIPService -Credential $cred
 Enable-AIPService
 
-$mailboxname = Read-Host "Mailbox name? I.e First Last > "
-Set-Mailbox -Identity $mailboxname -SingleItemRecoveryEnabled $true
+#$mailboxname = Read-Host "Mailbox name? I.e First Last > "
+#Set-Mailbox -Identity $mailboxname -SingleItemRecoveryEnabled $true
--- Fix-NLA.ps1
+++ Fix-NLA.ps1
@@ -1,13 +1,10 @@
 $DC_ServiceList = @("NSI","RpcSs","TcpIp","Dhcp","Eventlog","DNS","NTDS")
 $MachineType = ''
-
-Write-Output "`n`n[o] --- NLA FIX --- `nv0.2 - Service restart fix`n"
-
+Write-Output "`n`n[o] --- NLA FIX --- `nv0.3 - Set Firewallprofile after`n"
 Write-Output "[.] Checking Machine type.."
 $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 if ($osInfo.ProductType -eq 1) { $MachineType = 'Workstation' }
 if ($osInfo.ProductType -eq 3) { $MachineType = 'MemberServer' }
-
 if ($MachineType -eq '') {
     Write-Output "[.] Checking Services.."
     $DC=$false
@@ -24,12 +21,9 @@ if ($MachineType -eq '') {
       $MachineType = 'DC' 
     }
 }
-
 Write-Output "[!] Machine Type found: $MachineType`n"
 Write-Output "[.] Setting dependencies for NLA Service:"
 if ($MachineType -eq 'Workstation' -or $MachineType -eq 'MemberServer') {
-  # Write-Output "[!] Workstation/Member Server: Setting NLA Service to depend on the Netlogon, TCP/IP, and DNS client service"
-  # sc.exe config nlasvc depend=TcpIp/DNScache/Netlogon
   Write-Output "[!] Workstation/Member Server: Setting NLA Service to depend on the TCP/IP and DNS client service"
   sc.exe config nlasvc depend=TcpIp/DNScache
 } 
@@ -37,7 +31,6 @@ if ($MachineType -eq 'DC') {
   Write-Output "[!] DC: Setting NLA Service to depend on NSI/RpcSs/TcpIp/DHCP/Eventlog/DNS/NTDS"
   sc.exe config nlasvc depend=NSI/RpcSs/TcpIp/DHCP/Eventlog/DNS/NTDS
 }
-
 Write-Output "[.] Restarting NLA Service:"
 #net.exe stop nlasvc /y
 #net.exe start nlasvc
@@ -50,23 +43,51 @@ if ((Get-Service nlasvc).Status -eq 'Stopped') {
   Write-Output "[!] Error! couldn't stop/start NLA service, will try to kill the process here and make sure its restarted.."
   $nlasvc = sc.exe queryex nlasvc
   foreach ($line in $nlasvc) { if ($line -like "*PID*") { $nlapid = $line } }
-  Write-Host "[.] Found NLA service $($nlapid), killing:"
+  Write-Output "[.] Found NLA service $($nlapid), killing:"
   $nlapid2 = ($nlapid.split(":")[1].trim())
   taskkill /f /pid $($nlapid2)
   $nlapidOld = $nlapid2
   Start-Service Nlasvc -Verbose
-  Write-Host "[.] Waiting 5 seconds for service to restart.."
+  Write-Output "[.] Waiting 5 seconds for service to restart.."
   Start-Sleep 5
-  Write-Host "[.] Checking for new PID.."
+  Write-Output "[.] Checking for new PID.."
   $nlasvc = sc.exe queryex nlasvc
   foreach ($line in $nlasvc) { if ($line -like "*PID*") { $nlapid = $line } }
   $nlapid2 = ($nlapid.split(":")[1].trim())
   if ($nlapid2 -ne $nlapidOld) {
-    Write-Host "[.] Success! NLA service PID=$($nlapid2), Old PID=$($nlapidOld)"
+    Write-Output "[.] Success! NLA service PID=$($nlapid2), Old PID=$($nlapidOld)"
   } else {
-    Write-Host "[!] FAILED. PID=$($nlapid2), Old PID=$($nlapidOld)"
-    Write-Host "[!] Cannot fix, either you are not running as admin, or server needs a reboot possibly."
+    Write-Output "[!] FAILED. PID=$($nlapid2), Old PID=$($nlapidOld)"
+    Write-Output "[!] Cannot fix, either you are not running as admin, or server needs a reboot possibly."
   }
 }
-
+if ($MachineType -eq 'DC') {
+  Start-Sleep 1
+  $NetProfile = (Get-NetConnectionProfile).NetworkCategory
+  Write-Output "[.] Setting Firewall profile to DomainAuthenticated, currently $($NetProfile):"
+  Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory DomainAuthenticated
+  Start-Sleep 1
+  $NetProfile = (Get-NetConnectionProfile).NetworkCategory
+  if ($NetProfile -notlike '*Domain*') {
+    Write-Output "[!] Error setting Firewall profile:  SetNetConnectionProfile -NetworkCategory DomainAuthenticated, returned: $($NetProfile)"
+    Write-Output "[!] Exiting with errors!"
+    exit
+  } else {
+    Write-Output "[!] Set profile to: $($NetProfile)"
+  }
+} else {
+  Start-Sleep 1
+  $NetProfile = (Get-NetConnectionProfile).NetworkCategory
+  Write-Output "[.] Setting Firewall profile to Private, currently $($NetProfile):"
+  Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+  Start-Sleep 1
+  $NetProfile = (Get-NetConnectionProfile).NetworkCategory
+  if ($NetProfile -ne 'Private') {
+    Write-Output "[!] Error setting Firewall profile:  SetNetConnectionProfile -NetworkCategory Private, returned: $($NetProfile)"
+    Write-Output "[!] Exiting with errors!"
+    exit
+  } else {
+    Write-Output "[!] Set profile to: $($NetProfile)"
+  }
+}
 Write-Output "[o] Done!" 
