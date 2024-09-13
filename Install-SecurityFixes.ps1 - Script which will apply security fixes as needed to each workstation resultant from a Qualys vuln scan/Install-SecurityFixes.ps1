42c797df1ec82e61f56cb0b0f31125d022941fc5
Alex Datsko <alexdatsko@gmail.com>
2024-09-13 09:37:44 -0700

    Set up system to never go to sleep

--- Install-SecurityFixes.ps1 - Script which will apply security fixes as needed to each workstation resultant from a Qualys vuln scan/Install-SecurityFixes.ps1
+++ Install-SecurityFixes.ps1 - Script which will apply security fixes as needed to each workstation resultant from a Qualys vuln scan/Install-SecurityFixes.ps1
@@ -46,8 +46,8 @@ $AllHelp = "########################################################
 #### VERSION ###################################################
 
 # No comments after the version number on the next line- Will screw up updates!
-$Version = "0.40.09"
-# New in this version:   91764 - Microsoft Windows Codecs Library Web Media Extension Remote Code Execution Vulnerability, added fix for OfficeC2RClient.exe with start-process
+$Version = "0.40.10"
+# New in this version:   Set up system to never go to sleep 
 
 $VersionInfo = "v$($Version) - Last modified: 9/13/2024"
 
@@ -744,6 +744,132 @@ function Check-Reg {
   $ErrorActionPreference="Continue" # Set back to standard error termination setting
   return $checkvar
 }
+
+function Get-PowerScheme {
+	[CmdletBinding()][OutputType([object])]
+	param ()
+	
+	#Get the currently active power scheme
+	$Query = powercfg.exe /getactivescheme
+	#Get the alias name of the active power scheme
+	$ActiveSchemeName = ($Query.Split("()").Trim())[1]
+	#Get the GUID of the active power scheme
+	$ActiveSchemeGUID = ($Query.Split(":(").Trim())[1]
+	$Query = powercfg.exe /query $ActiveSchemeGUID
+	$GUIDAlias = ($Query | where { $_.Contains("GUID Alias:") }).Split(":")[1].Trim()
+	$Scheme = New-Object -TypeName PSObject
+	$Scheme | Add-Member -Type NoteProperty -Name PowerScheme -Value $ActiveSchemeName
+	$Scheme | Add-Member -Type NoteProperty -Name GUIDAlias -Value $GUIDAlias
+	$Scheme | Add-Member -Type NoteProperty -Name GUID -Value $ActiveSchemeGUID
+	Return $Scheme
+}
+
+function Set-PowerSchemeSettings {
+	[CmdletBinding()]
+	param
+	(
+		[string]
+		$MonitorTimeoutAC,
+		[string]
+		$MonitorTimeoutDC,
+		[string]
+		$DiskTimeoutAC,
+		[string]
+		$DiskTimeoutDC,
+		[string]
+		$StandbyTimeoutAC,
+		[string]
+		$StandbyTimeoutDC,
+		[string]
+		$HibernateTimeoutAC,
+		[string]
+		$HibernateTimeoutDC
+	)
+	
+	$Scheme = Get-PowerScheme
+	If (($MonitorTimeoutAC -ne $null) -and ($MonitorTimeoutAC -ne "")) {
+		Write-Host "Setting monitor timeout on AC to"$MonitorTimeoutAC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "monitor-timeout-ac" + [char]32 + $MonitorTimeoutAC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\7516b95f-f776-4464-8c53-06167f40cc99\3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e"
+		$TestValue = $MonitorTimeoutAC
+		$PowerIndex = "ACSettingIndex"
+	}
+	If (($MonitorTimeoutDC -ne $null) -and ($MonitorTimeoutDC -ne "")) {
+		Write-Host "Setting monitor timeout on DC to"$MonitorTimeoutDC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "monitor-timeout-dc" + [char]32 + $MonitorTimeoutDC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\7516b95f-f776-4464-8c53-06167f40cc99\3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e"
+		$TestValue = $MonitorTimeoutDC
+		$PowerIndex = "DCSettingIndex"
+	}
+	If (($DiskTimeoutAC -ne $null) -and ($DiskTimeoutAC -ne "")) {
+		Write-Host "Setting disk timeout on AC to"$DiskTimeoutAC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "disk-timeout-ac" + [char]32 + $DiskTimeoutAC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\0012ee47-9041-4b5d-9b77-535fba8b1442\6738e2c4-e8a5-4a42-b16a-e040e769756e"
+		$TestValue = $DiskTimeoutAC
+		$PowerIndex = "ACSettingIndex"
+	}
+	If (($DiskTimeoutDC -ne $null) -and ($DiskTimeoutDC -ne "")) {
+		Write-Host "Setting disk timeout on DC to"$DiskTimeoutDC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "disk-timeout-dc" + [char]32 + $DiskTimeoutDC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\0012ee47-9041-4b5d-9b77-535fba8b1442\6738e2c4-e8a5-4a42-b16a-e040e769756e"
+		$TestValue = $DiskTimeoutDC
+		$PowerIndex = "DCSettingIndex"
+	}
+	If (($StandbyTimeoutAC -ne $null) -and ($StandbyTimeoutAC -ne "")) {
+		Write-Host "Setting standby timeout on AC to"$StandbyTimeoutAC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "standby-timeout-ac" + [char]32 + $StandbyTimeoutAC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\238c9fa8-0aad-41ed-83f4-97be242c8f20\29f6c1db-86da-48c5-9fdb-f2b67b1f44da"
+		$TestValue = $StandbyTimeoutAC
+		$PowerIndex = "ACSettingIndex"
+	}
+	If (($StandbyTimeoutDC -ne $null) -and ($StandbyTimeoutDC -ne "")) {
+		Write-Host "Setting standby timeout on DC to"$StandbyTimeoutDC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "standby-timeout-dc" + [char]32 + $StandbyTimeoutDC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\238c9fa8-0aad-41ed-83f4-97be242c8f20\29f6c1db-86da-48c5-9fdb-f2b67b1f44da"
+		$TestValue = $StandbyTimeoutDC
+		$PowerIndex = "DCSettingIndex"
+	}
+	If (($HibernateTimeoutAC -ne $null) -and ($HibernateTimeoutAC -ne "")) {
+		Write-Host "Setting hibernate timeout on AC to"$HibernateTimeoutAC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "hibernate-timeout-ac" + [char]32 + $HibernateTimeoutAC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\238c9fa8-0aad-41ed-83f4-97be242c8f20\9d7815a6-7ee4-497e-8888-515a05f02364"
+		[int]$TestValue = $HibernateTimeoutAC
+		$PowerIndex = "ACSettingIndex"
+	}
+	If (($HibernateTimeoutDC -ne $null) -and ($HibernateTimeoutDC -ne "")) {
+		Write-Host "Setting hibernate timeout on DC to"$HibernateTimeoutDC" minutes....." -NoNewline
+		$Switches = "/change" + [char]32 + "hibernate-timeout-dc" + [char]32 + $HibernateTimeoutDC
+		$TestKey = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\" + $Scheme.GUID + "\238c9fa8-0aad-41ed-83f4-97be242c8f20\9d7815a6-7ee4-497e-8888-515a05f02364"
+		$TestValue = $HibernateTimeoutDC
+		$PowerIndex = "DCSettingIndex"
+	}
+	$ErrCode = (Start-Process -FilePath "powercfg.exe" -ArgumentList $Switches -WindowStyle Minimized -Wait -Passthru).ExitCode
+	$RegValue = (((Get-ItemProperty $TestKey).$PowerIndex) /60)
+	#Round down to the nearest tenth due to hibernate values being 1 decimal off
+	$RegValue = $RegValue - ($RegValue % 10)
+	If (($RegValue -eq $TestValue) -and ($ErrCode -eq 0)) {
+		Write-Host "Success" -ForegroundColor Yellow
+		$Errors = $false
+	} else {
+		Write-Host "Failed" -ForegroundColor Red
+		$Errors = $true
+	}
+	Return $Errors
+}
+
+function Set-PowerSettingsNeverSleep {
+  #Hardcoded Power Scheme Settings to never go to sleep on us, lets not change the monior, but disk timeout, sleep, and hibernate are now set to never
+#  $Errors = Set-PowerSchemeSettings -MonitorTimeoutAC 60
+#  $Errors = Set-PowerSchemeSettings -MonitorTimeoutDC 60
+  $Errors = Set-PowerSchemeSettings -DiskTimeOutAC 0
+  $Errors = Set-PowerSchemeSettings -DiskTimeOutDC 0
+  $Errors = Set-PowerSchemeSettings -StandbyTimeoutAC 0
+  $Errors = Set-PowerSchemeSettings -StandbyTimeoutDC 0
+  $Errors = Set-PowerSchemeSettings -HibernateTimeoutAC 0
+  $Errors = Set-PowerSchemeSettings -HibernateTimeoutDC 0
+}
+
+######################################### Script related
 
 function Check-ResultsForFiles {
     param(
@@ -2086,6 +2212,7 @@ if ($ServerName) {
 
 if (!$OnlyQIDs) {   # If we are not just trying a fix for one CSV, we will also see if we can install the Dell BIOS provider and set WOL to on, and backup Bitlocker keys to AD if possible
   if (Get-OSType -eq 1) {
+    Set-PowerSettingsNeverSleep  # Lets set this machine to never go to sleep, via registry. Disk, Sleep, and Hibernate time are set to 0.
     #Install-DellBiosProvider  # Will only run if value is set in Config
     #Set-DellBiosProviderDefaults # Will only run if value is set in Config  
   }
