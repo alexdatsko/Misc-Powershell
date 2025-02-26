d2abfc7e82ad1363fdba7da1f38870a6e1d6edc3
Alex Datsko <alexdatsko@gmail.com>
2025-02-26 07:41:13 -0800

    Fix API down issue

--- Fix-iDRAC400.ps1
+++ Fix-iDRAC400.ps1
@@ -4,7 +4,7 @@ $info = "#######################################
 # Fix-iDRAC400.ps1
 # Alex Datsko - MME Consulting Inc - alexd@mmeconsulting.com
 #   Sets the racadm set idrac.webserver.hostheadercheck 0 command via Redfish, without racadm 
-# v0.1 - 01-23-2025 - initial (NOT CURRENTLY WORKING!)"
+# v0.1 - 01-23-2025 - initial"
 
 
 $info
@@ -25,21 +25,18 @@ $password = Read-Host "Enter DRAC password"
 $combined = "$($username):$($password)"
 
 Write-Output "`n`n[.] Checking Webserver.1.HostHeaderCheck current settings .."
-curl.exe -isk -X GET -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1?`$select=Attributes/WebServer.1.HostHeaderCheck"
+curl.exe -sk -X GET -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1?`$select=Attributes/WebServer.1.HostHeaderCheck"
 
 Write-Output "`n`n[.] Setting Webserver.1.HostHeaderCheck to Disabled .."
+$data = "{\""Attributes\"":{\""WebServer.1.HostHeaderCheck\"":\""Disabled\""}}"
+$cmd = "curl.exe -sk -X PATCH -u ""$combined"" ""https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1"" -H ""Content-Type: application/json"" -d ""$data"""
+Write-Output "Command being sent: $cmd"
+$resp = (cmd.exe /c "$cmd")
+if ($resp -like "*request completed successfully*") {
+  Write-Output "[+] HostHeaderCheck is now disabled."
+} else {
+  Write-Output "[-] Something went wrong!  Error: `n`n  $resp `n`n" 
+}
 
-#Per the article :
-# https://github.com/dell/iDRAC-Redfish-Scripting/issues/303
-#curl.exe -k -X PATCH -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1" --insecure -d '{"Attributes":{"WebServer.1.HostHeaderCheck":"Disabled"}}' -i -H "Content-Type: application/json"
-# Still not working...
-curl.exe -isk -X PATCH -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1" -H "Content-Type: application/json" -d '{"Attributes":{"WebServer.1.HostHeaderCheck":false}}'
-
-#used to be:
-#{"Attributes":{"WebServer.1.HostHeaderCheck":"Enabled"}}
-
-#looks like is now:
-#{"@odata.context":"/redfish/v1/$metadata#DellAttributes.DellAttributes","@odata.id":"/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1","@odata.type":"#DellAttributes.v1_0_0.DellAttributes","Attributes":{"WebServer.1.HostHeaderCheck":"Disabled"}}
-
-Write-Output "`n`n[.] Checking Webserver.1.HostHeaderCheck current settings again.."
-curl.exe -isk -X GET -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1?`$select=Attributes/WebServer.1.HostHeaderCheck"
+Write-Output "`n`n[.] Checking Webserver.1.HostHeaderCheck current settings again just in case.."
+curl.exe -sk -X GET -u "$combined" "https://$($dracip):$($dracport)/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellAttributes/iDRAC.Embedded.1?`$select=Attributes/WebServer.1.HostHeaderCheck"
